169f9774dc03deed19d878b24dcb0ea527f5414e
Reed Terdal <reedterdal@gmail.com>
2019-11-01 17:39:39 -0400

    feat(Scanner): Added new Token Types for Phase 2
    * Added types to the tokenType enum
    * Added strings to tokenTypeStrings for new types

--- Parser/Token.h
+++ Parser/Token.h
@@ -15,7 +15,9 @@
 
 #include <glib.h>
 
-static const char * tokenTypeStrings[] ={"INVALID", "plus", "minus", "multiply", "divide", "power", "end_paren", "start_paren", "end_stmt", "assign", "integer", "float", "id", "string", "comma", "Double Type", "Int Type", "String Type"};
+static const char * tokenTypeStrings[] ={"INVALID", "plus", "minus", "multiply", "divide",
+                                         "power", "end_paren", "start_paren", "end_stmt", "assign", "integer", "float", "id", "string",
+                                         "comma", "Double Type", "Int Type", "String Type", "Less", "Less or equal", "equal", "not equal", "greater or equal", "greater", "for", "while", "if", "else", "start_bracket", "end_bracket"};
 
 char * __filePath;
 
@@ -24,6 +26,7 @@ char * __filePath;
  */
 typedef enum tokenType
 {
+    // Phase 1
     t_INVALID = 0,
     t_plus,
     t_minus,
@@ -41,7 +44,20 @@ typedef enum tokenType
     t_comma,
     t_type_double,
     t_type_integer,
-    t_type_string
+    t_type_string,
+    // Phase 2
+    t_comp_less,
+    t_comp_loe, // less than or equal to
+    t_comp_eq, // equal
+    t_comp_neq, // not equal
+    t_comp_goe, // greater than or equal to
+    t_comp_greater,
+    t_for,
+    t_while,
+    t_if,
+    t_else,
+    t_start_bracket,
+    t_end_bracket
 }tokenType;
 
 /**
