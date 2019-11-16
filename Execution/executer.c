12078f79af2e34cc6f43a3da5fc613d40a18bc9e
jxo7257 <jxo7257@rit.edu>
2019-11-16 18:59:37 -0500

    fixed execute, added funcionality for if, for, and while

--- Execution/executer.c
+++ Execution/executer.c
@@ -86,6 +86,15 @@ void execute(program * parse_tree)
             else if(current->statement->reassignment!= NULL){
                 eval_r_asmt(current->statement->reassignment);
             }
+            else if(current->statement->ifNode!= NULL){
+                eval_if(current->statement->ifNode);
+            }
+            else if(current->statement->forNode!= NULL){
+                eval_for(current->statement->forNode);
+            }
+            else if(current->statement->whileNode!= NULL){
+                eval_while(current->statement->whileNode);
+            }
         }
     }
     destroyGlobalScope();
